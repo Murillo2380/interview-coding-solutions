90fe56fb0e5a9031f09eeadee5fcf54dca4baf54
Murillo Ferreira <murillo.hpf@gmail.com>
2019-06-20 13:57:17 +0200

    Added O(n) solution

--- src/easy-facebook-best-multiply-in-array/js/Idea.md
+++ src/easy-facebook-best-multiply-in-array/js/Idea.md
@@ -3,3 +3,5 @@
 The idea is straight forward, we just need to test all the numbers. It is possible to optimize the algorithm a little bit by using memoization with the first 2 numbers in the multiplication chain.
 
 The running time of this algorithm is **O(n^3)** since we test `n` numbers against all the others `n-1` and `n-2` numbers in the array.
+
+Another solution is go through the array only once and keep tracking the **3 greatest positive numbers**, the **3 smaller negative numbers** and the **3 greatest negative numbers** and return the result considering if there is any positive number in the array or not. Using this strategy, the running time of the algorithm is **O(n)** since we only pass in the array once checking the possible results at the end.
--- src/easy-facebook-best-multiply-in-array/js/solution.js
+++ src/easy-facebook-best-multiply-in-array/js/solution.js
@@ -20,10 +20,106 @@ function solve(numbers) {
     return greater;
 }
 
+/**
+ *
+ * @param {number[]} numbers
+ */
+function solve2(numbers) {
+    let greaterIndexes = [-1, -1, -1];
+    let smallerNegativeIndexes = [-1, -1];
+    let greaterNegativeIndexes = [-1, -1, -1];
+
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > 0) {
+            if (
+                greaterIndexes[0] === -1 ||
+                numbers[greaterIndexes[0]] < numbers[i]
+            ) {
+                greaterIndexes[2] = greaterIndexes[1];
+                greaterIndexes[1] = greaterIndexes[0];
+                greaterIndexes[0] = i;
+            } else if (
+                greaterIndexes[1] === -1 ||
+                numbers[greaterIndexes[1]] < numbers[i]
+            ) {
+                greaterIndexes[2] = greaterIndexes[1];
+                greaterIndexes[1] = i;
+            } else if (
+                greaterIndexes[2] === -1 ||
+                numbers[greaterIndexes[2]] < numbers[i]
+            ) {
+                greaterIndexes[2] = i;
+            }
+
+            continue;
+        }
+
+        if (
+            smallerNegativeIndexes[0] === -1 ||
+            numbers[smallerNegativeIndexes[0]] > numbers[i]
+        ) {
+            smallerNegativeIndexes[1] = smallerNegativeIndexes[0];
+            smallerNegativeIndexes[0] = i;
+        } else if (
+            smallerNegativeIndexes[1] === -1 ||
+            numbers[smallerNegativeIndexes[1]] > numbers[i]
+        ) {
+            smallerNegativeIndexes[1] = i;
+        }
+
+        if (
+            greaterNegativeIndexes[0] === -1 ||
+            numbers[greaterNegativeIndexes[0]] < numbers[i]
+        ) {
+            greaterNegativeIndexes[2] = greaterNegativeIndexes[1];
+            greaterNegativeIndexes[1] = greaterNegativeIndexes[0];
+            greaterNegativeIndexes[0] = i;
+        } else if (
+            greaterNegativeIndexes[1] === -1 ||
+            numbers[greaterNegativeIndexes[1]] < numbers[i]
+        ) {
+            greaterNegativeIndexes[2] = greaterNegativeIndexes[1];
+            greaterNegativeIndexes[1] = i;
+        } else if (
+            greaterNegativeIndexes[2] === -1 ||
+            numbers[greaterNegativeIndexes[2]] < numbers[i]
+        ) {
+            greaterNegativeIndexes[2] = i;
+        }
+    }
+
+    if (greaterIndexes[0] === -1) {
+        // does not have any positive number in the array
+        return (
+            numbers[greaterNegativeIndexes[2]] *
+            numbers[greaterNegativeIndexes[1]] *
+            numbers[greaterNegativeIndexes[0]]
+        );
+    }
+
+    const option1 =
+        numbers[greaterIndexes[0]] *
+        smallerNegativeIndexes.reduce((total, i) => total * numbers[i], 1);
+
+    if (greaterIndexes.some(val => val === -1)) return option1;
+
+    const option2 = greaterIndexes.reduce((total, i) => total * numbers[i], 1);
+    return option1 > option2 ? option1 : option2;
+}
+
 const test = [-10, -10, 5, 2];
 const test2 = [-10, 10, 5, 2];
 const test3 = [-20, -10, -5, -2];
+const test4 = [-10, -9, -8, -7, -3, -2, -1, 0, 1, 2, 3, 20, 21, 22];
 
 console.log(solve(test)); // 500
 console.log(solve(test2)); // 100
 console.log(solve(test3)); // -100
+console.log(solve(test4)); // 9240
+
+console.log("=====");
+
+console.log(solve2(test)); // 500
+console.log(solve2(test2)); // 100
+console.log(solve2(test3)); // -100
+console.log(solve2(test4)); // 9240
