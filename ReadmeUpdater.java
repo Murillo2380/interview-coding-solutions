a4681e865f9c6e23199e07c33340f0286252049f
Murillo Ferreira <murillo.hpf@gmail.com>
2019-07-15 01:25:03 +0200

    Removed deadcode

--- ReadmeUpdater.java
+++ ReadmeUpdater.java
@@ -14,16 +14,6 @@ public class ReadmeUpdater {
     private static final String README_FILE_NAME = "./README.md";
     private static final String PROBLEMS_SECTION = "## Problems";
 
-    public void listFilesForFolder(final File folder) {
-        for (final File fileEntry : folder.listFiles()) {
-            if (fileEntry.isDirectory()) {
-                listFilesForFolder(fileEntry);
-            } else {
-                System.out.println(fileEntry.getName());
-            }
-        }
-    }
-
     private static class FilesGetter {
 
         private List<File> files;
